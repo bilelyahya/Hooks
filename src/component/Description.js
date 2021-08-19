4b1fa2f1605eed3cd544f2df283d939ad55d9787
bilel yahya <yahya.bilel@gmail.com>
2021-08-19 11:48:57 +0100

    router

--- src/component/Description.js
+++ src/component/Description.js
@@ -7,18 +7,20 @@ const Description = ({ movies, match }) => {
       {movies
         .filter((movie) => movie.name === match.params.name)
         .map((el, i) => (
-          <div key={i} className="card">
+          <div key={i} className="card-description">
             <h1>{el.name}</h1>
             <p>{el.desc}</p>
-            <iframe
-              width="560"
-              height="315"
-              src={el.trailer}
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
+            <span>
+              <iframe
+                width= "90%" 
+                height="315"
+                src={el.trailer}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </span><br></br>
             <Link to="/">
               <button className="button">Go back</button>
             </Link>
